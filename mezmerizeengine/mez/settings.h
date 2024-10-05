cd093d97d23066b7a1046edd26ae1d0cad8fb232
liquidjskeleton <108479299+jskeleton-fireal@users.noreply.github.com>
2024-10-05 12:03:38 -0500

    organized settings.h

--- mezmerizeengine/mez/settings.h
+++ mezmerizeengine/mez/settings.h
@@ -1,12 +1,6 @@
 #pragma once
 
-//this is very niche.. this was supposed to be supported from the start
-//but factories and whatnot make this a lot more complicated to set up
-//setting this to 0 basically stubs out everything that handles multiple
-//engines, which is faster to run code in which is GOOD!
-//potential use cases: client hosting servers, or something
-//The best way for this to work is to just have multiple programs running or something however that works i dont really care right now
-#define SUPPORT_MULTIPLE_ENGINES 0
+
 
 enum
 {
@@ -15,8 +9,14 @@ enum
 };
 
 
-#define RENDERABLE_MODE RENDER_MODE_OPENGL
+#define RENDER_MODE RENDER_MODE_OPENGL
 
 
 //support sfml's drawing tools. 0 by default
 #define RENDER_SUPPORT_SFML_GRAPHICS 0
+
+//alias
+#define RENDERABLE_MODE RENDER_MODE
+
+//ignore this. this will not be supported
+#define SUPPORT_MULTIPLE_ENGINES 0
