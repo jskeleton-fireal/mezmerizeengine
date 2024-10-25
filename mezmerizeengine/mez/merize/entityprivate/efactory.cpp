d5768f90aaa563b23d29857c606cc0665cb8a4ba
liquidjskeleton <108479299+jskeleton-fireal@users.noreply.github.com>
2024-10-25 16:05:46 -0500

    fixed a bug where entity factories would be freed before initialization

--- mezmerizeengine/mez/merize/entityprivate/efactory.cpp
+++ mezmerizeengine/mez/merize/entityprivate/efactory.cpp
@@ -3,13 +3,25 @@
 #error "efactory.cpp needs to be refactored to support multiple engines"
 #endif
 
+//internal efac identifier
 class efac_INTERNAL
 {
 public:
-	stdvector< efactory_t*> factories;
+	struct efac_it2
+	{
+		stdvector< efactory_t*> factories;
+	};
+public:
+	//Need to ensure this gets initialized before we start adding stuff to it
+	efac_it2* fac=0;
 	void upload(efactory_t* ref)
 	{
-		factories.push_back(ref);
+		if (!fac)
+		{
+			fac = new efac_it2();
+		}
+		//creating a new one here to make sure it doesnt get freed (This happens sometimes and im not sure why)
+		fac->factories.push_back(new efactory_t(*ref));
 	}
 };
 static efac_INTERNAL f;
@@ -32,12 +44,12 @@ efactory_t::efactory_t(const char* f_name, const mezcreatefunc_t* f_func, bool f
 
 efactory_t* entity_factories_t::find(const char* f_name)
 {
-	assert(f.factories.size());
-	for (int i = 0; i < f.factories.size(); i++)
+	assert(f.fac->factories.size());
+	for (int i = 0; i < f.fac->factories.size(); i++)
 	{
-		if (!strcmp(f.factories[i]->m_name, f_name))
+		if (!strcmp(f.fac->factories.at(i)->m_name, f_name))
 		{
-			return f.factories[i];
+			return f.fac->factories.at(i);
 		}
 	}
 	return nullptr;
