c23575bbba2b545e0131c5ca14fd1c7f2f9f40ab
liquidjskeleton <108479299+jskeleton-fireal@users.noreply.github.com>
2024-11-01 06:00:22 -0500

    added an entity_destroy global command

--- mezmerizeengine/mez/merize/data/entity_comms.cpp
+++ mezmerizeengine/mez/merize/data/entity_comms.cpp
@@ -7,7 +7,7 @@ CREATE_CONSOLECOMMAND_WFUNC(entity_create, 0)
 	if (!name.Valid()) return CONCMDRES_BADARGS;
 	char entdesired[128];
 	name.GetString(entdesired, 128);
-	console_printf("Test: %s\n", entdesired);
+	console_printf("attempting to create an instance of: %s\n", entdesired);
 
 	MezBaseEntity* ent = engine->CreateEntityByName<MezBaseEntity>(entdesired);
 	if (!ent)
@@ -19,5 +19,25 @@ CREATE_CONSOLECOMMAND_WFUNC(entity_create, 0)
 
 	ent->Spawn();
 	
+	return 0;
+}
+
+CREATE_CONSOLECOMMAND_WFUNC(entity_destroy, 0)
+{
+	auto name = f_args->Get(1);
+	if (!name.Valid()) return CONCMDRES_BADARGS;
+	char entdesired[128];
+	name.GetString(entdesired, 128);
+	console_printf("destroying the first instance of: %s\n", entdesired);
+
+	MezBaseEntity* ent = MezBaseEntity::Find_Typeless(entdesired);
+	if (!ent)
+	{
+		console_printf("Could not find an instance of \'%s\'\n", entdesired);
+		return 0;
+
+	}
+
+	ent->Destroy();
 	return 0;
 }
