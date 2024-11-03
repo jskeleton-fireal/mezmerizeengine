227666bb27e0b7ee5cafcc8a1e0f9fd9f2d3ef6f
liquidjskeleton <108479299+jskeleton-fireal@users.noreply.github.com>
2024-11-03 00:06:23 -0500

    added lookat function to mezangles

--- mezmerizeengine/mez/merize/data/mezangles.cpp
+++ mezmerizeengine/mez/merize/data/mezangles.cpp
@@ -1,5 +1,12 @@
+#include <mez/merize/merize.h> //make sure this is included first
 #include "mezangles.h"
+#include "mezvector.h"
+#include <mez/merize/console/console.h>
 
+const float pi = 3.1415926535897932384626433832795f;
+const float halfpi = pi / 2.0f;
+const float qpi = pi / 4.0f;
+const float dpi = pi * 2.0f;
 float degtorad(float deg) { return deg * 0.0174533f; }
 float radtodeg(float rad) { return rad * 57.2958f; }
 
@@ -16,3 +23,14 @@ float MezAngles::GetEuler(int index)
 {
     return radtodeg(m_angles[index]);
 }
+
+
+void MezAngles::LookAt(Vector f_position_localspace)
+{
+
+    f_position_localspace.normalize_inplace();
+    m_angles[ANGLE_PITCH] = -sinf(f_position_localspace.y);
+    m_angles[ANGLE_YAW] = atan2f(-f_position_localspace.x, f_position_localspace.z);
+    m_angles[ANGLE_ROLL] = 0.0f;
+}
+
--- mezmerizeengine/mez/merize/data/mezangles.h
+++ mezmerizeengine/mez/merize/data/mezangles.h
@@ -10,6 +10,7 @@ enum
 	ANGLE_ROLL, //sidways movement
 };
 struct MezAngles;
+struct Vector;
 typedef MezAngles MezAngles_Euler;
 struct MezAngles
 {
@@ -22,4 +23,6 @@ struct MezAngles
 	MezAngles() { m_angles[0] = 0; m_angles[1] = 0; m_angles[2] = 0; }
 	MezAngles(float p,float y,float r) { m_angles[0] = p; m_angles[1] = y; m_angles[2] = r; }
 	MezAngles(float p,float y) { m_angles[0] = p; m_angles[1] = y; m_angles[2] = 0; }
+
+	void LookAt(Vector f_position_localspace);
 };
