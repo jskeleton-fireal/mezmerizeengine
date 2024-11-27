da2cdbd0c89f1bb2816fc3cd89f425ad0b21e27b
liquidjskeleton <108479299+jskeleton-fireal@users.noreply.github.com>
2024-11-27 03:11:29 -0600

    added math operators to vector2 datatype

--- mezmerizeengine/mez/merize/data/mezvector2.h
+++ mezmerizeengine/mez/merize/data/mezvector2.h
@@ -6,4 +6,17 @@ struct Vector2
 {
 	float x;
 	float y;
+	Vector2 operator-(Vector2 i) { return Vector2(x - i.x, y - i.y); }
+	Vector2 operator+(Vector2 i) { return Vector2(x + i.x, y + i.y); }
+	Vector2 operator*(Vector2 i) { return Vector2(x * i.x, y * i.y); }
+	Vector2 operator/(Vector2 i) { return Vector2(x / i.x, y / i.y); }
+	Vector2 operator*(float i) { return Vector2(x * i, y * i); }
+	Vector2 operator/(float i) { return Vector2(x / i, y / i); }
+	Vector2 operator-() { return *this * -1; }
+	Vector2 operator-=(Vector2 i) { return *this = *this - i; }
+	Vector2 operator+=(Vector2 i) { return *this = *this + i; }
+	Vector2 operator*=(Vector2 i) { return *this = *this * i; }
+	Vector2 operator/=(Vector2 i) { return *this = *this / i; }
+	Vector2 operator*=(float i) { return *this = *this * i; }
+	Vector2 operator/=(float i) { return *this = *this / i; }
 };
