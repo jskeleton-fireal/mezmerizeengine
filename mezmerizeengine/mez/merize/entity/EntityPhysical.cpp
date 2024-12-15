0cff4b9ea2274e1aa934919efeb9546564feca4f
liquidjskeleton <108479299+jskeleton-fireal@users.noreply.github.com>
2024-12-15 06:29:05 -0600

    changed dynamic casts to static in entityphysical

--- mezmerizeengine/mez/merize/entity/EntityPhysical.cpp
+++ mezmerizeengine/mez/merize/entity/EntityPhysical.cpp
@@ -5,14 +5,14 @@
 
 void MezEntityPhysical::SetModel(const char* name)
 {
-	MezComponent_Renderable* l_renderable= dynamic_cast<MezComponent_Renderable*>(m_ComponentContainer.FindRenderable());
+	MezComponent_Renderable* l_renderable= static_cast<MezComponent_Renderable*>(m_ComponentContainer.FindRenderable());
 	assert(l_renderable);
 	l_renderable->m_Renderable->SetModel_Name(name);
 }
 
 int MezEntityPhysical::PrecacheModel(const char* name)
 {
-	MezComponent_Renderable* l_renderable = dynamic_cast<MezComponent_Renderable*>(m_ComponentContainer.FindRenderable());
+	MezComponent_Renderable* l_renderable = static_cast<MezComponent_Renderable*>(m_ComponentContainer.FindRenderable());
 	assert(l_renderable);
 	return l_renderable->m_Renderable->PrecacheModel_Name(name);
 }
@@ -20,20 +20,20 @@ int MezEntityPhysical::PrecacheModel(const char* name)
 void MezEntityPhysical::InitModel(const char* name)
 {
 	int precache_result = PrecacheModel(name);
-	if (precache_result == -1) return;
+	//if (precache_result == -1) return;
 	SetModel(name);
 }
 
 void MezEntityPhysical::SetModel(RModel_Base* raw_model)
 {
-	MezComponent_Renderable* l_renderable = dynamic_cast<MezComponent_Renderable*>(m_ComponentContainer.FindRenderable());
+	MezComponent_Renderable* l_renderable = static_cast<MezComponent_Renderable*>(m_ComponentContainer.FindRenderable());
 	assert(l_renderable);
 	l_renderable->m_Renderable->SetModel(raw_model);
 }
 
 void MezEntityPhysical::initialize2()
 {
-	MezComponent_Renderable* l_renderable = dynamic_cast<MezComponent_Renderable*>(m_ComponentContainer.FindRenderable());
+	MezComponent_Renderable* l_renderable = static_cast<MezComponent_Renderable*>(m_ComponentContainer.FindRenderable());
 	l_renderable->m_Renderable->m_rInterface->RequestFeature(RINTERFACE_FEATURE_3D);
 }
 
